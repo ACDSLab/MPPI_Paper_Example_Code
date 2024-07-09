7e0f1136832ac2a1c57e636a91fb991b516a8d69
Bogdan Vlahov <bvlahov3@gatech.edu>
2024-07-09 11:58:24 -0400

    Clean up minimal example

--- src/minimal_example.cu
+++ src/minimal_example.cu
@@ -2,7 +2,6 @@
 #include <mppi/cost_functions/cartpole/cartpole_quadratic_cost.cuh>
 #include <mppi/dynamics/cartpole/cartpole_dynamics.cuh>
 #include <mppi/feedback_controllers/DDP/ddp.cuh>
-#include <mppi_paper_example/controllers/CEM/cem_controller.cuh>
 
 const int NUM_TIMESTEPS = 100;
 const int NUM_ROLLOUTS = 2048;
@@ -12,15 +11,12 @@ using COST_T = CartpoleQuadraticCost;
 using FB_T = DDPFeedback<DYN_T, NUM_TIMESTEPS>;
 using SAMPLING_T = mppi::sampling_distributions::GaussianDistribution<DYN_T::DYN_PARAMS_T>;
 using CONTROLLER_T = VanillaMPPIController<DYN_T, COST_T, FB_T, NUM_TIMESTEPS, NUM_ROLLOUTS, SAMPLING_T>;
-// using CONTROLLER_T = CEMController<DYN_T, COST_T, FB_T, NUM_TIMESTEPS, NUM_ROLLOUTS, SAMPLING_T>;
 using CONTROLLER_PARAMS_T = CONTROLLER_T::TEMPLATED_PARAMS;
 
 int main(int argc, char** argv) {
   float dt = 0.02;
-  // set up dynamics
-  std::shared_ptr<DYN_T> dynamics = std::make_shared<DYN_T>();
-  // set up cost
-  std::shared_ptr<COST_T> cost = std::make_shared<COST_T>();
+  std::shared_ptr<DYN_T> dynamics = std::make_shared<DYN_T>();  // set up dynamics
+  std::shared_ptr<COST_T> cost = std::make_shared<COST_T>();    // set up cost
   // set up feedback controller
   std::shared_ptr<FB_T> fb_controller = std::make_shared<FB_T>(dynamics.get(), dt);
   // set up sampling distribution
@@ -37,9 +33,7 @@ int main(int argc, char** argv) {
   std::shared_ptr<CONTROLLER_T> controller = std::make_shared<CONTROLLER_T>(
       dynamics.get(), cost.get(), fb_controller.get(), sampler.get(), controller_params);
 
-  // set up initial state
-  DYN_T::state_array x = dynamics->getZeroState();
-
+  DYN_T::state_array x = dynamics->getZeroState();  // set up initial state
   // calculate control
   controller->computeControl(x, 1);
   auto control_sequence = controller->getControlSeq();
